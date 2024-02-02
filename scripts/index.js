6b938590f5db32d9e189429bade37510536115d4
Clement Chau <clementtjuwito2@gmail.com>
2024-02-02 20:15:48 +0700

    done home page

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,53 @@
+// Header
+
 document.addEventListener('DOMContentLoaded', function () {
     var header = document.getElementById('header');
-    var scrollThreshold = 60; // Adjust this value as needed
+    var headerimg = document.getElementById('headerimg');
+    var header_a = document.querySelectorAll('.header-a');
+    var scrollThreshold = 56; // Adjust this value as needed
 
     window.addEventListener('scroll', function () {
         var scrollPosition = window.scrollY;
-        console.log(scrollPosition);
 
         if (scrollPosition > scrollThreshold) {
             header.classList.add('solid');
+            headerimg.src = "images/logos/manford-blue.png";
+            header_a.forEach( (a) => {
+              if (!a.classList.contains('header-active')) {
+                a.style.color = "black";
+              }
+            })
         } else {
             header.classList.remove('solid');
+            headerimg.src = "images/logos/manford-white.png";
+            header_a.forEach ( (a) => {
+              if (!a.classList.contains('header-active')) {
+                a.style.color = "white";
+              }
+            })
         }
     });
 });
 
+// Sliders Button
+  const productsButton = document.querySelectorAll('.products-button');
+  productsButton.forEach((button) => {
+    button.addEventListener('click', () => {
+      window.location.href = './pages/products.html';
+    })
+  })
+
+// Image Manford Logo in header as button
+  const headerLogo = document.getElementById('headerimg');
+  headerLogo.addEventListener('click', () => {
+    window.location.href = './index.html';
+  })
+
+
+
+
+
+// Sliders
 
 
 let slideIndex = 1;
@@ -50,3 +84,11 @@ function showSlides(n) {
 setInterval(function() {
   plusSlides(1); // Change to next slide
 }, 6000); // Adjust the interval as needed (in milliseconds)
+
+
+
+
+
+
+
+
